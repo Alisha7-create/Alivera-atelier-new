295c8cf9eabc04f4f40130c8f7508c50fdf8f1c3
Alisha7-create <iamalisha.00007@gmail.com>
2026-08-14 12:23:02 +0530

    Refactor session handler to read user session

--- api/auth/session.js
+++ api/auth/session.js
@@ -1,5 +1,4 @@
-export const access='public'; export const methods=['GET'];
-export default async function(req,res){ res.json({user:req.user||null}); }import { readSession } from '../../src/compat.js';
+import { readSession } from '../../src/compat.js';
 
 export const access = 'public';
 export const methods = ['GET'];
