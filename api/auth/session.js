48a63aacdcf0849b89d04d33d3f8599d94cd1be5
Alisha7-create <iamalisha.00007@gmail.com>
2026-08-14 12:16:31 +0530

    Refactor session endpoint to read user session

--- api/auth/session.js
+++ api/auth/session.js
@@ -1,2 +1,10 @@
 export const access='public'; export const methods=['GET'];
-export default async function(req,res){ res.json({user:req.user||null}); }
+export default async function(req,res){ res.json({user:req.user||null}); }import { readSession } from '../../src/compat.js';
+
+export const access = 'public';
+export const methods = ['GET'];
+
+export default async function(req, res) {
+  const user = await readSession(req);
+  res.json({ user });
+}
