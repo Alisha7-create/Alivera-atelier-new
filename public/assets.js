e65978fcf3e0b79a277c1d5439d92c4cf2bc7ebb
Alisha7-create <iamalisha.00007@gmail.com>
2026-08-21 19:10:01 +0530

    Define ALIVERA_ASSETS with image paths

--- public/assets.js
+++ public/assets.js
@@ -1,1 +1,10 @@
-window.ALIVERA_ASSETS={};
+window.ALIVERA_ASSETS = {
+    lavender: '/images/lavender-light-top.jpg',
+    lavender_chart: '/images/lavender-chart.jpg',
+    sunset: '/images/sunset-breeze.jpg',
+    sunset_chart: '/images/sunset-chart.jpg',
+    emerald: '/images/emerald-pearl.jpg',
+    emerald_chart: '/images/emerald-chart.jpg',
+    stardust: '/images/stardust-eclipse-gown.jpg',
+    stardust_chart: '/images/stardust-chart.jpg'
+};
